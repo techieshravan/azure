e6ae262568a203d004bde4d5c5796a1e903cde11
Shravan <shravan.kasagoni@outlook.com>
2015-11-01 00:03:55 +0530

    DocumentDB

---
+++ DocumentDB/todo/src/Models/Context.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace todo.Models
+{
+    public class Context : DbContext
+    {
+        public Context() : base("name=Context")
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+    
+    }
+}
