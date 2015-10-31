44be85f267a9f72a741cd269f2ea4931428bb838
Shravan <shravan.kasagoni@outlook.com>
2015-10-31 23:58:44 +0530

    SignalR Scaleout

---
+++ ServiceBus/SignalRScaleout/SignalRChat/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Owin;
+using Microsoft.Owin;
+using Microsoft.AspNet.SignalR;
+
+[assembly: OwinStartup(typeof(SignalRChat.Startup))]
+namespace SignalRChat
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            string connectionString = "Endpoint=sb://signalrscaleout1.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=jqZb9xKYuymL7wOlNV6wDIu/qlttPk3ENL4OAxHdzGA=";
+            GlobalHost.DependencyResolver.UseServiceBus(connectionString, "Chat");
+            app.MapSignalR();
+        }
+    }
+}
